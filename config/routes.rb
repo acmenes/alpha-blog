ce7ecfb1dc6748967ce7f5f39341bf13130f16ee
acmenes <acmenescompositions@gmail.com>
2016-01-15 23:33:26 +0000

    update on Jan 15

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :articles
   resources :users
   
